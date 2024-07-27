fb5715f6005e62745be221b07b2f18b96bf09624
Stephen Mwaura <stevecartel44@gmail.com>
2024-07-27 13:35:04 +0300

    corrections for task 4 file

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,3 @@
 USE alx_book_store;
 
-FROM information_schema.COLUMNS,
-SELECT
-`COLUMN_NAME`
-`COLUMN_TYPE`
-`TABLE_SCHEMA`
-`TABLE_NAME` = 'books'
+INFORMATION_SCHEMA.COLUMNS,TABLE_SCHEMA = 'alx_book_store', TABLE_NAME = 'Books'
