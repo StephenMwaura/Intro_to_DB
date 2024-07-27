7b69fc6bd8bb7fb3c0578e9f140671726eaaeced
Stephen Mwaura <stevecartel44@gmail.com>
2024-07-27 12:47:05 +0300

    added the alx book store file

---
+++ MySQLServer.py
@@ -0,0 +1,17 @@
+import mysql.connector
+import getpass
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = getpass.getpass("Enter password: ")
+
+)
+mycursor = mydb.cursor()
+try:
+ query = "Create database if not exists alx_book_store"
+ mycursor.execute(query)
+ print("Database alx_book_store created successfully!")
+except ConnectionError:
+ print("Failed to connect to alx_book_store")
+
+mydb.close()
